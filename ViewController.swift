ffd023896a3caaba385fb73081219d6d75170019
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2023-09-26 07:36:36 -0700

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  weather app
+//
+//  Created by Immanuel Matthews-Feemster on 9/26/23.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
+    
+    @IBOutlet var table: UITableView!
+    
+    let locationManager = CLLocationManager()
+    
+    var currentLocation: CLLocation?
+    
+    var models = [Weather]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        table.register(HourlyTableViewCell.nib(), forCellReuseIdentifier: HourlyTableViewCell.identifier)
+        table.register(WeatherTableViewCell.nib(), forCellReuseIdentifier: WeatherTableViewCell.identifier)
+        
+        table.delegate = self
+        table.dataSource = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        setupLocation()
+    }
+    
+    func setupLocation() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if !locations.isEmpty, currentLocation == nil {
+            currentLocation = locations.first
+            locationManager.stopUpdatingLocation()
+            requestWeatherForLocation()
+        }
+    }
+    
+    func requestWeatherForLocation() {
+        guard let currentLocation = currentLocation else { return }
+        let long = currentLocation.coordinate.longitude
+        let lat = currentLocation.coordinate.latitude
+        
+        print("\(long) & \(lat)")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    struct Weather {
+        
+    }
+
+}
+
---
+++ WeatherCell/WeatherTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherTableViewCell.swift
+//  weather app
+//
+//  Created by Immanuel Matthews-Feemster on 9/26/23.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    static let identifier = "WeatherTableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "WeatherTableViewCell", bundle: nil)
+    }
+}
